3b2531bb17cf28cbb63374f7e30405b9a11ccf40
Stanislav Polos <polos_s@groupbwt.com>
2022-05-03 16:58:31 +0300

    feat: add table books_categories

--- app/database/models/__init__.py
+++ app/database/models/__init__.py
@@ -2,3 +2,4 @@ from .user import User
 from .category import Category
 from .subcategory import SubCategory
 from .book import Book
+from .book_category import BookCategory
---
+++ app/database/models/book_category.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column
+from sqlalchemy.dialects.mysql import BIGINT
+
+from database.models.base import Base
+from database.models.mixins import MysqlPrimaryKeyMixin, MysqlTimestampsMixin
+
+
+class BookCategory(Base, MysqlPrimaryKeyMixin, MysqlTimestampsMixin):
+    __tablename__ = "books_categories"
+
+    subcategory_id = Column(BIGINT(unsigned=True), index=True, nullable=False, unique=True)
+    book_id = Column(BIGINT(unsigned=True), index=True, nullable=False, unique=True)
