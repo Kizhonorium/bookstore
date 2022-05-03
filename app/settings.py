cb79a0eb65fe4fbe603d14cdccd891382beabf7b
Stanislav Polos <polos_s@groupbwt.com>
2022-05-03 15:57:10 +0300

    feat: add settings

---
+++ app/settings.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
+DB_PORT = int(os.getenv("DB_PORT", "3306"))
+DB_USERNAME = os.getenv("DB_USERNAME", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_DATABASE = os.getenv("DB_DATABASE", "db_name")
